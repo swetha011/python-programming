c646014a92e6d568f4afbefbe7cfa1665922fc9d
swetha011 <36623617+swetha011@users.noreply.github.com>
2018-02-20 20:18:01 +0530

    Create alphabet or not.py

---
+++ beginner/alphabet or not.py
@@ -0,0 +1,9 @@
+def main():
+    a=raw_input("enter the input")
+    if((a>='a' and a<='z')or(a>='A' and a<='Z')):
+        print("alphabet\n")
+    else:
+        print("not alphabet")
+
+if __name__ == '__main__':
+    main()
