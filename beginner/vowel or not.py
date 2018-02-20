4ab79d9e6d53cf1647bb9a574c1eba2cdbdcbabd
swetha011 <36623617+swetha011@users.noreply.github.com>
2018-02-20 15:56:47 +0530

    Create vowel or not.py

---
+++ beginner/vowel or not.py
@@ -0,0 +1,9 @@
+def main():
+    a=raw_input("enter a character")
+    if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
+        print("vowel")
+    else:
+        print("consonant")
+
+if __name__ == '__main__':
+    main()
