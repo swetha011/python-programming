8b18456084ab55192b07031854f0e8f07f0d70df
swetha011 <36623617+swetha011@users.noreply.github.com>
2018-02-20 14:14:05 +0530

    Create fibonaaci series.py

---
+++ player/fibonaaci series.py
@@ -0,0 +1,13 @@
+def main():
+    a=-1
+    b=1
+    print("fibonacci series")
+    for i in range(0,10):
+        list=a+b
+        print(list)
+        a=b
+        b=list
+
+
+if __name__ == '__main__':
+    main()
