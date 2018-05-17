d0ac510fc59e836a83f0fe5cf083de37bfccbd08
swetha011 <36623617+swetha011@users.noreply.github.com>
2018-05-17 10:07:16 +0530

    Update positive or negative or zero.py

--- beginner/positive or negative or zero.py
+++ beginner/positive or negative or zero.py
@@ -1,8 +1,9 @@
-num = float(input("Enter a number: "))
-if num >= 0:
+num=float(input("enter the number"))
+if num >=0:
    if num == 0:
-       print("Zero")
-   else:
-       print("Positive number")
-else:
-   print("Negative number")
+      print("zero")
+    else:
+      print("positive number:")
+    else:
+      print("negative number:")
+         
